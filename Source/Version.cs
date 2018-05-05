74dc98be2a846c5e89438eef45d6a74a2c301330
Lisias T <me@lisias.net>
2018-05-05 20:11:23 -0300

    Better Version and Dependencies control

---
+++ Source/Version.cs
@@ -0,0 +1,12 @@
+﻿namespace CrewLight
+{
+	static class Version
+	{
+		public const int major = 1;
+		public const int minor = 15;
+		public const int patch = 0;
+		public const int build = 0;
+		public const string Number = "1.15.0.0";
+		public const string Text = Number + " Alpha";
+	}
+}
