b7615d5ece8b469bb296e0e52c836e61874d9327
Lisias T <me@lisias.net>
2019-09-29 16:07:05 -0300

    Adding KSPe Log facilities

---
+++ Source/Log.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using KSPe.Util.Log;
+using System.Diagnostics;
+
+namespace CrewLight
+{
+	internal static class Log
+	{
+		private static readonly Logger log = Logger.CreateForType<LightDirector> ();
+
+		internal static void init ()
+		{
+			log.level =
+#if DEBUG
+				Level.TRACE
+#else
+				Level.INFO
+#endif
+				;
+		}
+
+		internal static void force (string msg, params object [] @params)
+		{
+			log.force (msg, @params);
+		}
+
+		internal static void info (string msg, params object [] @params)
+		{
+			log.info (msg, @params);
+		}
+
+		internal static void warn (string msg, params object [] @params)
+		{
+			log.warn (msg, @params);
+		}
+
+		internal static void detail (string msg, params object [] @params)
+		{
+			log.detail (msg, @params);
+		}
+
+		internal static void error (string msg, params object [] @params)
+		{
+			log.error (msg, @params);
+		}
+
+		[ConditionalAttribute ("DEBUG")]
+		internal static void dbg (string msg, params object [] @params)
+		{
+			log.trace (msg, @params);
+		}
+	}
+}
