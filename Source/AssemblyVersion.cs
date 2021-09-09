d9e4883b634abbc8a36c4cdcf431a42b4bb94f21
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-09-09 13:53:41 -0400

    Added AssemblyFileVersion Updated version file for 1.12

--- Source/AssemblyVersion.cs
+++ Source/AssemblyVersion.cs
@@ -6,3 +6,4 @@
  using System.Reflection;
 
  [assembly: AssemblyVersion("1.20.0.0")]
+ [assembly: AssemblyFileVersion("1.20.0.0")]
--- deploy.bat
+++ deploy.bat
@@ -12,9 +12,12 @@ set GAMEDIR=CrewLight
 set GAMEDATA="GameData"
 set VERSIONFILE=%GAMEDIR%.version
 
+set DP0=r:\dp0\kspdev
+
 copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\Plugins"
+copy /Y "%1%3".pdb "%GAMEDATA%\%GAMEDIR%\Plugins"
+
 copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 
 xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
-
-pause
+xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%DP0%\GameData\%GAMEDIR%"
