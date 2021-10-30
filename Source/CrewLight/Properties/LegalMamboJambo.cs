24b4d600b8a56bee08a8be7bfb9abf683428d197
Lisias T <me@lisias.net>
2021-10-30 09:16:57 -0300

    Missing miscelannea details

--- Source/CrewLight/Properties/LegalMamboJambo.cs
+++ Source/CrewLight/Properties/LegalMamboJambo.cs
@@ -2,7 +2,7 @@
 {
 	public static class LegalMamboJambo
 	{
-		public const string Company = "L Aerospace/KSP Division";
+		public const string Company = "/L Aerospace/KSP Division";
 		public const string Copyright = "© 2021 LisiasT";
 		public const string Product = "CrewLight";
 		public const string Trademark = "CrewLight™ by Li0n, /L Unofficial™ by LisiasT";
